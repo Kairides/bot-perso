88a344937e19efeb41a174787e6969daa54125ff
Kairides <gueguen.ronan1@gmail.com>
2020-11-13 17:19:33 +0100

    check on insults moved out and new command for youtube search

--- server.js
+++ server.js
@@ -1,15 +1,19 @@
 const {Client, MessageEmbed, Message, MessageReaction} = require('discord.js');
+const ytdl = require('ytdl-core');
+const ytsr = require('ytsr');
+const prefix = require('./config.json');
 
 const accessKeyId = process.env.key_discord;
-
 const client = new Client();
 client.login(accessKeyId);
 
-var playing = false;
+const broadcast = client.voice.createBroadcast()
 
-const MOTS_INTERDITS = ["connard", "connards", "salauds", "salaud", "salopard", "salopards", "salope", "salopes", "bite", "couilles", "merde", "chiasse", "putain", 
+//var playing = false;
+
+const MOTS_INTERDITS = ["connard", "connards", "salauds", "salaud", "salopard", "salopards", "salope", "salopes", "couilles", "merde", "chiasse", "putain", 
 			"bordel", "foutre", "con", "cons", "couille", "batard", "batards", "bougnoul", "enculé", "enflure", "branleur", "pute", "putes", "gouine",
-		       "gouines", "pd", "pédé", "pouffiasse", "puoffiasses", "lopette", "nègre", "negre", "negres", "nègres", "mongol", "mongols", "nazi", "nazis",
+		       "gouines", "pd", "pédé", "pouffiasse", "pouffiasses", "lopette", "nègre", "negre", "negres", "nègres", "mongol", "mongols", "nazi", "nazis",
 			"négro", "négros", "nique", "niquer", "trouduc", "Rob Schneider"];
 
 function checkMot(mot){
@@ -22,10 +26,23 @@ function checkMot(mot){
     return grossier;
 };
 
-function resetPlaying(){
-    playing = false;
-    //console.log("Playing is false !")
-};
+function checkMessage(message){
+    var mots = message.content.split(" ");
+    var grossier = false;
+    mots.forEach(mot => {
+        if (checkMot(mot) != ""){grossier = true;}
+    });
+
+    if(grossier){
+        var msg = new Message();
+        msg.content = "le contenu de votre message a été jugé inapproprié et a donc été supprimé.";
+        message.reply(msg);
+        message.delete({ timeout: 100 })
+            .then(msg => console.log(`Deleted message from ${msg.author.username} after 0.1 seconds`))
+            .catch(console.error);
+    }
+
+}
 
 client.on('ready', () =>{
 	console.log("I am ready !");
@@ -33,26 +50,42 @@ client.on('ready', () =>{
 
 client.on('message', message =>{
 
-    /*if(message.channel.name === "general" && message.content.startsWith('!createSession')){
-        playing = true;
-        setTimeout(resetPlaying, 360000);
-        console.log("Running !")
-    }*/
+    checkMessage(message.content);
 
-    var mots = message.content.split(" ");
-    mots.forEach(mot => {
-        if (checkMot(mot) != ""){
-            var msg = new Message();
-            msg.content = "le contenu de votre message a été jugé inapproprié et a donc été supprimé.";
+    if(!message.content.startsWith(prefix) || message.author.bot)return;
+    
+    var args = message.content.slice(prefix.length).trim().split(" ");
+    const command = args.shift().toLowerCase();
 
-            message.reply(msg);
-            // message.member.setNickname(message.member.nickname + "(GROSSIER)");
-            message.delete({ timeout: 100 })
-            .then(msg => console.log(`Deleted message from ${msg.author.username} after 0.1 seconds`))
-            .catch(console.error);
+    if(command === "play"){
+        if(!args.length){
+            message.reply("paramètres de recherche manquants. Pour que je joue un morceau," +
+            "veuillez taper la commande ```!play``` suivie de ce que vous voulez que je joue.\n"+
+            "exemple: ```!play akroasis```");
+        }else{
+            var search = args.join(" ");
+            
+            ytsr.getFilters('github').then(async (filters1) => {
+                const filter1 = filters1.get('Type').find(o => o.name === 'Video');
+                const filters2 = await ytsr.getFilters(filter1.ref);
+                const filter2 = filters2.get('Duration').find(o => o.name.startsWith('Short'));
+                const options = {
+                    limit: 5,
+                    nextpageRef: filter2.ref,
+                }
+                const searchResults = await ytsr(null, options);
+                dosth(searchResults);
+            }).catch(err => {
+                console.error(err);
+            });
         }
-    });
+    }
 });
+
+/*function resetPlaying(){
+    playing = false;
+    //console.log("Playing is false !")
+};*/
 
 /*client.on('messageReaction', messageReaction =>{
     if(message.channel.name === "info-serveur"){
